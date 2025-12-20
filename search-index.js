0ee0196253e21b6909ea9627e2d8e50214ccfdc0
BBertcode <arthur.bert@gmail.com>
2025-12-20 21:37:39 +0100

    Add files via upload

---
+++ search-index.js
@@ -0,0 +1,123 @@
+window.SEARCH_INDEX = [
+  {
+    "title": "Anti-TNF alpha",
+    "url": "antitnf.html",
+    "tokens": [
+      "Anti-TNF alpha",
+      "antitnf",
+      "anti-tnf",
+      "adalimumab",
+      "infliximab",
+      "amgevita",
+      "remsima",
+      "biosimilaire",
+      "quantiferon"
+    ]
+  },
+  {
+    "title": "Azathioprine",
+    "url": "azathioprine.html",
+    "tokens": [
+      "Azathioprine",
+      "azathioprine",
+      "imurel",
+      "6-tgn",
+      "6-mmp",
+      "metabolites"
+    ]
+  },
+  {
+    "title": "Corticothérapie",
+    "url": "cortico.html",
+    "tokens": [
+      "Corticothérapie",
+      "cortico",
+      "prednisone",
+      "corticoide",
+      "decroissance",
+      "palier"
+    ]
+  },
+  {
+    "title": "Accueil",
+    "url": "index.html",
+    "tokens": [
+      "Bienvenue sur InternoClic",
+      "index",
+      "accueil",
+      "internoclic"
+    ]
+  },
+  {
+    "title": "Méthotrexate",
+    "url": "mtx.html",
+    "tokens": [
+      "Méthotrexate",
+      "mtx",
+      "methotrexate",
+      "mtx",
+      "acide folique"
+    ]
+  },
+  {
+    "title": "Mycophénolate mofétil",
+    "url": "mycophenolate.html",
+    "tokens": [
+      "Mycophénolate mofétil",
+      "mycophenolate",
+      "mmf",
+      "cellcept",
+      "myfortic"
+    ]
+  },
+  {
+    "title": "Ostéoporose",
+    "url": "osteoporose.html",
+    "tokens": [
+      "Ostéoporose",
+      "osteoporose",
+      "vitamine d",
+      "calcium",
+      "dexa",
+      "densitometrie"
+    ]
+  },
+  {
+    "title": "Tocilizumab",
+    "url": "tocilizumab.html",
+    "tokens": [
+      "Tocilizumab",
+      "tocilizumab",
+      "roactemra",
+      "tyenne",
+      "tocilizumab",
+      "bilan lipidique",
+      "crp",
+      "nfp",
+      "quantiferon"
+    ]
+  },
+  {
+    "title": "Uvéites",
+    "url": "uveite.html",
+    "tokens": [
+      "Uvéites",
+      "uveite",
+      "uveite",
+      "uvéite"
+    ]
+  },
+  {
+    "title": "Vaccination",
+    "url": "vaccins.html",
+    "tokens": [
+      "Vaccination",
+      "vaccins",
+      "vaccination",
+      "grippe",
+      "pneumocoque",
+      "zona",
+      "covid"
+    ]
+  }
+];
